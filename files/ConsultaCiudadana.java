9b97122f0cbfb20c6bccf5a79bcaf4e4b08ddf35
AlejandroIRD <alejandro.rivera.d@mail.pucv.cl>
2025-08-27 22:43:40 -0400

    cambio en la clase consulta ciudadana

--- files/ConsultaCiudadana.java
+++ files/ConsultaCiudadana.java
@@ -1,12 +1,45 @@
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConsultaCiudadana {
-    public ConsultaCiudadana(String nombre, LocalDate fecha, String descripcion) {}
-    public void agregarTema(Tema tema) {}
-    public void agregarTema(String nombre, String descripcion) {}
-    public String getNombre() { return ""; }
-    public List<Tema> getTemas() { return null; }
-    public void mostrarTemas() {}
-    public String toString() { return ""; }
+    private String nombre;
+    private LocalDate fecha;
+    private String  descripcion;
+    private List<Tema> temas;
+
+
+    public ConsultaCiudadana(String nombre, LocalDate fecha, String descripcion) {
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.descripcion = descripcion;
+        List<tema> temas = new ArrayList<>();
+    }
+
+    public void agregarTema(Tema tema) {
+        temas.add(tema);
+    }
+
+    public void agregarTema(String nombre, String descripcion) {
+        temas.add(new tema(nombre, descripcion));
+    }
+
+    public String getNombre() { 
+        return nombre; 
+        }
+
+    public List<Tema> getTemas() { 
+        return temas;
+    }
+
+    public void mostrarTemas() {
+        system.out.println("Consulta:" + nombre);
+        for (int i = 0; i < temas.size(); i++) {
+            System.out.println((i + 1) + ". " + temas.get(i));
+        }
+    }
+
+    public String toString() {
+         return nombre + " - " + fecha + " (" + temas.size() + " temas)"; 
+    }
 }
